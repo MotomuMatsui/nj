2d8b6674a9c36f968b5794112a0f91b883c8e6a3
motomumatsui <qm@bs.s.u-tokyo.ac.jp>
2018-11-16 23:12:35 +0900

    TBE

--- src/messages.cpp
+++ src/messages.cpp
@@ -31,9 +31,9 @@ void print_banner(){
 }
 
 void print_usage(char*& program){
-  cerr << "Usage: " << program << " [-e INTEGER(>=0)] [-r INTEGER(>0)] [-s] [-l] [-h] [-v] input > output" << endl;
+  cerr << "Usage: " << program << " [-e INTEGER(>=0)] [-b STRING(tbe/fbs)] [-r INTEGER(>0)] [-s] [-l] [-h] [-v] input > output" << endl;
   cerr << "-e " << "the number of replicates for EP method. Default: 0" << endl;
-  cerr << "-b " << "the bootstrap method, tbe or fbs Default: tbe" << endl;
+  cerr << "-b " << "the bootstrap method, tbe or fbs. Default: tbe" << endl;
   cerr << "-r " << "the random seed number for EP method. Default: random number" << endl;
   cerr << "-s " << "silent mode: do not report progress. Default: Off" << endl;
   cerr << "-l " << "newick format with actual names. Default: Off" << endl;
